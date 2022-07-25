8b0c0a31cb7cecf9b5d461c952b8f80bfb7b1084
blaze1402 <axelblaze14022000@gmail.com>
2022-07-25 16:16:33 +0530

    Added register page and created a form file to manage all the forms in the app

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from forms import LoginForm, RegisterForm
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///notes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SECRET_KEY']='135d77340c66b6df976c29df'
 db=SQLAlchemy(app)
 
 class Note(db.Model):
@@ -16,8 +18,11 @@ class Note(db.Model):
     def __repr__(self) -> str:
         return f"{self.Sno} - {self.title}"
 
+#homepage of the website
 @app.route('/', methods=['GET', 'POST'])
 def index_page():
+
+    #Whenever a post request is received then this if block will execute and save a note to the database
     if request.method=='POST':
         title=request.form['title']
         desc=request.form['desc']
@@ -26,8 +31,27 @@ def index_page():
         db.session.add(note)
         db.session.commit()
         
+    #otherwise the saved notes will be shown to the users
     allNotes=Note.query.all()
     return render_template("index.html", allNotes=allNotes)
+
+#registration page of the website
+@app.route('/register')
+def register_page():
+    form=RegisterForm()
+    if form.validate_on_submit:
+        form.username
+
+    return render_template("register.html", form=form)
+
+#login page of the website
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    form=LoginForm()
+    if form.validate_on_submit:
+        pass
+
+    return render_template("login.html", form=form)
 
 @app.route('/edit/<int:sno>', methods=['GET', 'POST'])
 def edit(sno):
