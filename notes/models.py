50e2f8446ca93547e48642a51325a380c845b1ff
blaze1402 <axelblaze14022000@gmail.com>
2022-08-03 19:35:14 +0530

    Added login manager and flash messages

--- notes/models.py
+++ notes/models.py
@@ -1,11 +1,15 @@
-from notes import db
+from notes import db, login_manager
+from flask_login import UserMixin
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
-    username = db.Column(db.String(length=31), nullable=True, unique=True)
-    email = db.Column(db.String(length=50), nullable=True, unique=True)
-    password_hash = db.Column(db.String(length=60), nullable=True)
+    username = db.Column(db.String(length=31), nullable=False, unique=True)
+    email = db.Column(db.String(length=50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=False)
     notes = db.relationship("Note", backref="saved_notes", lazy=True)
 
     def __repr__(self) -> str:
--- notes/routes.py
+++ notes/routes.py
@@ -1,5 +1,5 @@
-from flask_login import login_user
-from flask import redirect, render_template, request, url_for
+from flask_login import login_user, logout_user
+from flask import flash, redirect, render_template, request, url_for
 from datetime import datetime
 from notes.forms import LoginForm, RegisterForm
 from notes import app, db
@@ -24,16 +24,27 @@ def index_page():
 
 
 # registration page of the website
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register_page():
+
+    #created a RegisterForm object as form
     form = RegisterForm()
-    if form.validate_on_submit:
+
+    #this condition is used to create a user when the validation of the form is completed this is done on the submit button of the form
+    if form.validate_on_submit():
         create_user=User(username=form.username.data,
                          email=form.email.data,
                          password_hash=form.password.data)
         db.session.add(create_user)
         db.session.commit()
-        # login_user(create_user)
+        login_user(create_user)
+        flash(f'Account created successfully! You are now logged in as {create_user.username}!', category='success')
+        return redirect(url_for('index_page'))
+
+    #this condition displays the errors done while submitting the form
+    if form.errors!={}:
+        for errors in form.errors.values():
+            flash(f"There was an error with creating a user: {errors}", category='danger')
     return render_template("register.html", form=form)
 
 
@@ -41,10 +52,23 @@ def register_page():
 @app.route("/login", methods=["GET", "POST"])
 def login_page():
     form = LoginForm()
-    if form.validate_on_submit:
-        pass
+    if form.validate_on_submit():
+        attempted_user=User.query.filter_by(username=form.username.data).first()
+        attempted_password=User.query.filter_by(password_hash=form.password.data).first()
+        if attempted_user and attempted_password:
+            login_user(attempted_user)
+            flash(f'You have been successfully logged in as {attempted_user.username}!', category='success')
+            return redirect(url_for('index_page'))
+        else:
+            flash('Username and password do not match! Please try again.', category='danger')
 
     return render_template("login.html", form=form)
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash(f'You have been logged out!', category='info')
+    return redirect(url_for('index_page'))
 
 
 @app.route("/edit/<int:sno>", methods=["GET", "POST"])
